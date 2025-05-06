17b1535df0caa2c0cba0445e5cdd29c4b501400c
Olgica04 <82290208+Olgica04@users.noreply.github.com>
2025-05-06 17:22:53 +0200

    Add files via upload

---
+++ AbstractAndInterface/Task2-Homework/Task2.Domain/Models/Circle.cs
@@ -0,0 +1,21 @@
+﻿
+using Task2.Domain.Interface;
+
+namespace Task2.Domain.Models
+{
+    public class Circle : IShape
+    {
+        public const double Pi = 3.14;
+        public double Radius { get; set; }
+
+        public Circle (double radius)
+        {
+            Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Pi * Radius * Radius;
+        }
+    }
+}
