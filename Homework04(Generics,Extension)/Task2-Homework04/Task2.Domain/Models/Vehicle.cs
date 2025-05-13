b65b4e6e63ddc4bb2d88f8699b7f1f25eacdc251
Olgica04 <82290208+Olgica04@users.noreply.github.com>
2025-05-13 23:01:06 +0200

    Add files via upload

---
+++ Homework04(Generics,Extension)/Task2-Homework04/Task2.Domain/Models/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Task2.Domain.Models
+{
+    public abstract class Vehicle
+    {
+        public int Id { get; set; }
+
+        public abstract void DisplayInfo();
+    }
+}
---
+++ Homework04(Generics,Extension)/Task2-Homework04/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using Task2.Domain.Models;
+
+Vehicle car = new Car();
+Vehicle plane = new Airplane();
+Vehicle boat = new Boat();
+Vehicle motorBike = new MotorBike();
+
+car.DisplayInfo();
+plane.DisplayInfo();
+boat.DisplayInfo();
+motorBike.DisplayInfo();
+
+Car driveCar = new Car();
+Airplane flyPlane = new Airplane();
+Boat sailBoat = new Boat();
+MotorBike driveMotorBike = new MotorBike();
+
+driveCar.Drive();
+flyPlane.Fly();
+sailBoat.Sail();
+driveMotorBike.Wheelie();
