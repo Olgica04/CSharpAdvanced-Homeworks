62702657ee5660d679a753e27370039d47deb5fd
Olgica04 <82290208+Olgica04@users.noreply.github.com>
2025-05-13 21:49:57 +0200

    Add files via upload

---
+++ Homework03(Static,Polymorphism)/Task2-Homework03/Task2.Domain/Models/Boat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Domain.Models
+{
+    public class Boat : Vehicle
+    {
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("I am a boat and I do not have wheels!");
+        }
+    }
+}
---
+++ Homework03(Static,Polymorphism)/Task2-Homework03/Task2.Domain/Models/MotorBike.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Domain.Models
+{
+    public class MotorBike : Vehicle
+    {
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("I am a motorbike and I drive on 2 wheels!");
+        }
+    }
+}
