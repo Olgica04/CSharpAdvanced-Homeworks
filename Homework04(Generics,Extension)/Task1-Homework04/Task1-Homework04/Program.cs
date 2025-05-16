d002883c7c054bd55fbd932536fee37787feffea
Olgica04 <82290208+Olgica04@users.noreply.github.com>
2025-05-16 16:45:28 +0200

    Add files via upload

---
+++ Homework04(Generics,Extension)/Task1-Homework04/Task1-Homework04/Program.cs
@@ -0,0 +1,7 @@
+﻿using Task1.Domain;
+
+var names = new List<string> { "Alice", "Bob", "Charlie", "John", "Eva" };
+PrintInConsole.PrintCollection(names);
+
+PrintInConsole.Print("Steven");
+PrintInConsole.Print(123);
